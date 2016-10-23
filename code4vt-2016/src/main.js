bb31cada32b511a5f276ca4c5ccd726715ad73c1
Stefan Stefanov <stefan.bg@gmail.com>
2016-10-23 12:02:00 +0300

    wip

---
+++ code4vt-2016/src/main.js
@@ -0,0 +1,6 @@
+System.import('hakimel/reveal.js').then(function (reveal) {
+    reveal.initialize();
+});
+
+System.import('hakimel/reveal.js/css/reveal.css!');
+System.import('hakimel/reveal.js/css/theme/white.css!');
